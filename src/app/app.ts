e766f1e3a6a8361a4bbd8d1ba3c18925aa6ab970
Krystian S. <krystian.szofer@s2023.tu-chemnitz.de>
2026-05-14 13:59:42 +0200

    Added Social Links & Hover Colors.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core'
 import { RouterLink, RouterModule, RouterOutlet } from '@angular/router'
 import { Navbar } from './shared/navbar/navbar'
+import { SocialLinks } from './shared/social-links/social-links'
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet, Navbar, RouterLink, RouterModule],
+    imports: [RouterOutlet, Navbar, RouterLink, RouterModule, SocialLinks],
     templateUrl: './app.html',
     styleUrl: './app.scss',
 })
--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -4,10 +4,19 @@ import { Hero } from '../hero/hero'
 import { AboutMe } from '../about-me/about-me'
 import { Projects } from '../projects/projects'
 import { RouterLink, RouterModule } from '@angular/router'
+import { SocialLinks } from '../shared/social-links/social-links'
 
 @Component({
     selector: 'app-home',
-    imports: [Navbar, Hero, AboutMe, Projects, RouterLink, RouterModule],
+    imports: [
+        Navbar,
+        Hero,
+        AboutMe,
+        Projects,
+        RouterLink,
+        RouterModule,
+        SocialLinks,
+    ],
     templateUrl: './home.html',
     styleUrl: './home.scss',
 })
