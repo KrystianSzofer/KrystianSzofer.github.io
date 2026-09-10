0b685e91ab4ae02f5f19ba9030a7aaeab508decb
Krystian <krystianszofer7@gmail.com>
2026-09-10 20:32:14 +0200

    Added logos.

--- src/app/about-me/about-me.ts
+++ src/app/about-me/about-me.ts
@@ -1,9 +1,28 @@
 import { Component } from '@angular/core'
 
+const logoNames = [
+    'HTML5',
+    'CSS3',
+    'TS',
+    'Angular',
+    'React',
+    'Cplusplus',
+    'Csharp',
+    'Blender',
+    'Gimp',
+    'Canva',
+    'Figma',
+    'Java',
+    'GitLab',
+    'Jira',
+]
+
 @Component({
     selector: 'app-about-me',
     imports: [],
     templateUrl: './about-me.html',
     styleUrl: './about-me.scss',
 })
-export class AboutMe {}
+export class AboutMe {
+    logos = logoNames
+}
